e546cb5bcfc602d25faab0b2f5b694f9ee55845f
GotinhaAzul <d4v1santiago@gmail.com>
2025-12-29 13:22:00 -0300

    Add new fishing pools with varied odds

---
+++ pools/vereda_musgosa.py
@@ -0,0 +1,32 @@
+NOME_POOL = "Vereda Musgosa"
+
+RARIDADES = [
+    ("Comum", 0.45),
+    ("Incomum", 0.38),
+    ("Raro", 0.15),
+    ("Lendário", 0.02),
+]
+
+PEIXES = {
+    "Comum": [
+        "Lambari Musgoso",
+        "Carpa Pantanosa",
+        "Tilápia Turva",
+        "Bagre Lodoso",
+    ],
+    "Incomum": [
+        "Truta Musgosa",
+        "Piau Camuflado",
+        "Traíra de Junco",
+        "Bicuda dos Brejos",
+    ],
+    "Raro": [
+        "Dourado do Brejo",
+        "Enguia de Névoa",
+        "Peixe-Lança Turvo",
+    ],
+    "Lendário": ["Guardião do Charco", "Sombra do Pântano"],
+}
+
+VALOR_MULT = 0.88
+NIVEL_MIN = 1
