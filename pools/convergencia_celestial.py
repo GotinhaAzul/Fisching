4cbfd4b065da11115e3bb42d4b42641bf7ecff98
GotinhaAzul <d4v1santiago@gmail.com>
2026-01-04 14:04:15 -0300

    Tone down legendary rates and rewards for late pools

---
+++ pools/convergencia_celestial.py
@@ -0,0 +1,35 @@
+NOME_POOL = "Convergência Celestial"
+
+RARIDADES = [
+    ("Comum", 0.39),
+    ("Incomum", 0.35),
+    ("Raro", 0.23),
+    ("Lendário", 0.03),
+]
+
+PEIXES = {
+    "Comum": [
+        "Peixe-Estrela Pálido",
+        "Carpa Celeste",
+        "Lambari de Neblina",
+        "Tilápia Galáctica",
+    ],
+    "Incomum": [
+        "Truta Meteoro",
+        "Pirarucu Boreal",
+        "Pacu de Plasmas",
+        "Matrinxã do Cometa",
+    ],
+    "Raro": [
+        "Enguia de Aurora",
+        "Dourado Constelar",
+        "Lula de Estase",
+    ],
+    "Lendário": [
+        "Koi das Constelações",
+        "Leviatã Astral",
+    ],
+}
+
+VALOR_MULT = 1.2
+NIVEL_MIN = 36
